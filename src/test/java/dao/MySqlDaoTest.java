dd898926ca2262a0f986fc16415c30f8aa0711c5
e.korotaeva <yamansarova_elena@mail.ru>
2016-12-29 04:05:38 +0500

    java doc

---
+++ src/test/java/dao/MySqlDaoTest.java
@@ -0,0 +1,26 @@
+package dao;
+
+import ru.innopolis.course3.Pojo.User;
+import ru.innopolis.course3.dao.DaoFactory;
+import ru.innopolis.course3.dao.DataException;
+import ru.innopolis.course3.dao.Identified;
+import ru.innopolis.course3.dao.UniversalDao;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runners.Parameterized;
+import ru.innopolis.course3.mysql.MySqlDaoFactory;
+
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Collection;
+
+
+/**
+ * Created by korot on 29.12.2016.
+ */
+public class MySqlDaoTest  {
+
+
+}
